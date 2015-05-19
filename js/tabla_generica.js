ce5af3f08952ec1973431a89c3b1d6e0fb8ce58b
verena91 <vereojeda@gmail.com>
2015-05-19 17:56:09 -0400

    Descarga de notificaciones filtradas.

--- js/tabla_generica.js
+++ js/tabla_generica.js
@@ -92,33 +92,124 @@ function draw_table(columns, url, id_tabla) {
 
 	$('tfoot').insertAfter('thead');
 
-	/* Apply the request for CSV download */
-	$("#btn-descarga").on(
+	/* Apply the request for JSON download */
+	$("#btn-descarga-json").on(
 			'click',
 			function() {
+				startLoading();
 				var oTable = $(id_tabla).dataTable();
 				var oParams = oTable.oApi
 						._fnAjaxParameters(oTable.fnSettings());
-				// console.log(oParams);
-				$.ajax({
-					type : 'GET',
-					//url : 'http://localhost:9000/datos/tab/planificaciones?'
-						//	+ $.param(oParams),
-					url : url + '?' + $.param(oParams),
-					beforeSend : function(xhr) {
-						xhr.setRequestHeader('Accept', 'application/csv');
-					},
-					success : function(response) {
-						/** Definir de donde sacar el nombre para el archivo de descarga **/
-						var nameAux = url.split("/");
-						var name = nameAux[nameAux.length - 1];
-						// console.log(name);
-						download(response, name + ".csv", "text/csv");
-					},
-					error : function() {
-						console.log("error");
-					}
+				//console.log(oParams.columns);
+				columnFilters = oParams.columns
+				tieneFiltro = false;
+				_.each(columnFilters, function(filter) {
+				  if (filter.search.value != "")
+				        tieneFiltro = true;
 				});
+				if (tieneFiltro) {
+					$.ajax({
+						type : 'GET',
+						//url : 'http://localhost:9000/datos/tab/planificaciones?'
+							//	+ $.param(oParams),
+						url : url + '?' + $.param(oParams),
+						beforeSend : function(xhr) {
+							xhr.setRequestHeader('Accept', 'application/csv');
+						},
+						success : function(response) {
+							/** Definir de donde sacar el nombre para el archivo de descarga **/
+							var nameAux = url.split("/");
+							var name = nameAux[nameAux.length - 1];
+							// console.log(name);
+							download(response, name + ".json", "text/json");
+							finishedLoading();
+						},
+						error : function() {
+							console.log("error");
+							finishedLoading();
+							alert("Ha ocurrido un error.");
+						}
+					});
+				} else {
+					finishedLoading();
+					alert("Debe indicar al menos un filtro para poder realizar la descarga.");
+				}
+				
+			});
+
+		/* Apply the request for CSV download */
+	$("#btn-descarga-csv").on(
+			'click',
+			function() {
+				startLoading();
+				var oTable = $(id_tabla).dataTable();
+				var oParams = oTable.oApi
+						._fnAjaxParameters(oTable.fnSettings());
+				//console.log(oParams.columns);
+				columnFilters = oParams.columns
+				tieneFiltro = false;
+				_.each(columnFilters, function(filter) {
+				  if (filter.search.value != "")
+				        tieneFiltro = true;
+				});
+				if (tieneFiltro) {
+					$.ajax({
+						type : 'GET',
+						//url : 'http://localhost:9000/datos/tab/planificaciones?'
+							//	+ $.param(oParams),
+						url : url + '?' + $.param(oParams),
+						beforeSend : function(xhr) {
+							xhr.setRequestHeader('Accept', 'application/csv');
+						},
+						success : function(response) {
+							/** Definir de donde sacar el nombre para el archivo de descarga **/
+							var nameAux = url.split("/");
+							var name = nameAux[nameAux.length - 1];
+							// console.log(name);
+							JSONData = response;
+			                var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+			                var CSV = '';
+			                var row = "";
+			                // This loop will extract the label from 1st index of on array
+			                for ( var index in arrData[0]) {
+			                    // Now convert each value to string and comma-seprated
+			                    row += index + ',';
+			                }
+			                row = row.slice(0, -1);
+			                // append Label row with line break
+			                CSV += row + '\r\n';
+			                // 1st loop is to extract each row
+			                for (var i = 0; i < arrData.length; i++) {
+			                    var row = "";
+
+			                    // 2nd loop will extract each column and convert it in string
+			                    // comma-seprated
+			                    for ( var index in arrData[i]) {
+			                        row += '"' + arrData[i][index] + '",';
+			                    }
+			                    row.slice(0, row.length - 1);
+			                    // add a line break after each row
+			                    CSV += row + '\r\n';
+			                }
+
+			                if (CSV == '') {
+			                    alert("Invalid data");
+			                    return;
+			                }
+							download(CSV, name + ".csv", "text/csv");
+							finishedLoading();
+
+						},
+						error : function() {
+							console.log("error");
+							finishedLoading();
+							alert("Ha ocurrido un error.");
+						}
+					});
+				} else {
+					finishedLoading();
+					alert("Debe indicar al menos un filtro para poder realizar la descarga.");
+				}
 			});
 	
 	$(id_tabla).on( 'draw.dt', function () {
